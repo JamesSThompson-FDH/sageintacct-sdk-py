22774d39dcc8b1eb35d9c7912496f83913d29dd5
JamesSThompson-FDH <96254994+JamesSThompson-FDH@users.noreply.github.com>
2022-05-03 16:21:16 -0400

    Update and rename gl_account_grp_heir.py to gl_acct_grp_hier.py

--- sageintacctsdk/apis/gl_acct_grp_hier.py
+++ sageintacctsdk/apis/gl_acct_grp_hier.py
@@ -6,7 +6,7 @@ from typing import Dict
 from .api_base import ApiBase
 
 
-class AccountGrpHeir(ApiBase):
+class GLAcctGrpHier(ApiBase):
     """Class for Accounts APIs."""
     def __init__(self):
         ApiBase.__init__(self, dimension='GLACCTGRPHIERARCHY')
