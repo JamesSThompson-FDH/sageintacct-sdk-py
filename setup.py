b916405842580ea4355b06c0a38f7e8d7ef9cfc7
JamesSThompson-FDH <96254994+JamesSThompson-FDH@users.noreply.github.com>
2022-05-06 11:44:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='sageintacctsdk',
-    version='0.1.1',
+    version='v0.1.2fdh',
     author='Ashwin T',
     author_email='ashwin.t@fyle.in',
     description='Python SDK for accessing Sage Intacct APIs',
